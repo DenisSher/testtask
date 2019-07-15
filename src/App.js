90ff8a8f7bed462c2f8b9f5413ae38f6f5e4b053
DenisSher <denis.shershakov.1995@gmail.com>
2019-07-15 14:15:16 +0300

    made basic todo

--- src/App.js
+++ src/App.js
@@ -1,40 +1,63 @@
 import React, { useState, useEffect } from "react";
 import TodoList from "./components/TodoList";
+import uuidv4 from "uuid/v4";
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const WrapperCheckbox = styled.div`
+  display: flex;
+`;
 
 function GuestApp() {
   const [items, setItems] = useState([]);
   const [text, setText] = useState("");
   const [couple, setCouple] = useState(false);
 
+  useEffect(
+    () => setItems(JSON.parse(localStorage.getItem("storage")) || []),
+    []
+  );
+
   const handleChange = e => {
     setText(e.target.value);
   };
+
+  const handleKeyPress = e => {
+        if (e.key === "Enter") {
+            text === "" ||
+            setItems([...items, { id: uuidv4(), name: text, couple }]);
+            setText("");
+            localStorage.setItem("storage", JSON.stringify(items));
+        }
+    };
 
   return (
     <>
       <span>
         Amount of guests:
-        {items.reduce((acc, current) => {
+        {JSON.parse(localStorage.getItem("storage")).reduce((acc, current) => {
           return current.couple === true ? acc + 2 : acc + 1;
         }, 0)}
       </span>
       <Wrapper>
-        <input id="new-todo" value={text} onChange={e => handleChange(e)} />
+        <input value={text} onChange={e => handleChange(e)}
+           onKeyPress={handleKeyPress}
+        />
         <p>With a couple</p>
-        <input id="couple" type="checkbox" onClick={() => setCouple(!couple)} />
-        <button
-          onClick={() => {
-            setItems([...items, { name: text, couple }]);
-            setText("");
-          }}
-        >
-          Add guest
-        </button>
+        <input type="checkbox" onClick={() => setCouple(!couple)} />
       </Wrapper>
-      <TodoList items={items}/>
+        <p>Filter</p>
+        <WrapperCheckbox>
+            <input type="checkbox"
+                   onClick={() => setItems(JSON.parse(localStorage.getItem("storage")) || [])}
+            /><span>All</span>
+            <input type="checkbox"/><span>With couple</span>
+            <input type="checkbox"/><span>Single</span>
+        </WrapperCheckbox>
+      <TodoList />
     </>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const GuestList = styled.ul`
@@ -12,22 +12,43 @@ const Guest = styled.li`
   color: black;
 `;
 
-const Item = styled.input`
+const Item = styled.input` 
   outline: none;
   background: white;
   border: none;
 `;
 
-const TodoList = ({ items, onFilter }) => (
-  <GuestList>
-    {items.map((item, index) => (
-      <Guest>
-        <Item value={item.name} key={item.id} disabled={true} />
-        <span>{item.couple === true ? 'With couple' : "Single"}</span>
-        <button>Delete</button>
-      </Guest>
-    ))}
-  </GuestList>
-);
+const TodoList = () => {
+  const [guests, setGuests] = useState(JSON.parse(localStorage.getItem("storage")));
+  useEffect(
+      () => setGuests(JSON.parse(localStorage.getItem("storage")) || []),
+      []
+  );
+  return (
+    <GuestList>
+      {guests.map((item) => (
+        <Guest>
+          <Item
+            value={item.name}
+            disabled={true}
+            onDoubleClick={() => {console.log(1)}}
+          />
+          <span>{item.couple === true ? "With couple" : "Single"}</span>
+          <button
+            onClick={() => {
+              localStorage.setItem(
+                "storage",
+                JSON.stringify(guests.filter(x => x.id !== item.id))
+              );
+              setGuests(JSON.parse(localStorage.getItem("storage")));
+            }}
+          >
+            Delete
+          </button>
+        </Guest>
+      ))}
+    </GuestList>
+  );
+};
 
 export default TodoList;
